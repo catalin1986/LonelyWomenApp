54a8526fc2c6af1ec318110be7d31030754d013b
catalin1986 <34644999+catalin1986@users.noreply.github.com>
2017-12-23 15:56:14 +0200

    new drawable

--- app/src/main/java/date/lonelywomen/lonelywomen18/MainActivity.java
+++ app/src/main/java/date/lonelywomen/lonelywomen18/MainActivity.java
@@ -1,35 +1,25 @@
 package date.lonelywomen.lonelywomen18;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdRequest.Builder;
 import com.google.android.gms.ads.AdView;
-
+import date.lonelywomen.lonelywomen18.R;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
-
     private AdView mAdView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String url ="http://lonelywomen.date/register";
-        WebView view=(WebView) this.findViewById(R.id.webView);
+        setContentView((int) R.layout.activity_main);
+        WebView view = (WebView) findViewById(R.id.webView);
         view.getSettings().setJavaScriptEnabled(true);
         view.setWebViewClient(new WebViewClient());
-        view.loadUrl(url);
-
-        mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
+        view.loadUrl("http://lonelywomen.date/register");
+        this.mAdView = (AdView) findViewById(R.id.adView);
+        this.mAdView.loadAd(new Builder().build());
     }
 }
-
